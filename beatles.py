202944071c7db978663cbb310fc0a43e9d7bece4
aiko-isnt-ded <viviana.toledo@iteso.mx>
2023-11-03 08:05:49 -0600

    Quitar \n con strip

--- beatles.py
+++ beatles.py
@@ -1,6 +1,4 @@
 file = open("data/beatles.txt", "r", encoding="utf8")
 for line in file:
-    for caracter in line:
-        print(repr(caracter), end=" ")
-    print()
+    print(line.strip())
 file.close()
